e41c75ce942f4eb50cc1e4856ce05dcf07699c04
Zach Hardesty <hello@zachhardesty.com>
2020-09-18 17:16:06 -0500

    chore: clean up code, release v1.0.0

--- extension.js
+++ extension.js
@@ -1,5 +1,10 @@
 const vscode = require("vscode")
 
+// regexes
+const JSDOC_START_TAG = /\/\*\*\s?/
+const JSDOC_END_TAG = /\s?\*\//
+const JSDOC_LINE_CHAR = /\s\*\s/
+
 /**
  * @param {vscode.TextLine | number} line - input
  * @returns {vscode.TextLine} output
@@ -8,6 +13,7 @@ const getPrevLine = (line) =>
   vscode.window.activeTextEditor.document.lineAt(
     (typeof line !== "number" ? line.lineNumber : line) - 1
   )
+
 /**
  * @param {vscode.TextLine | number} line - input
  * @returns {vscode.TextLine} output
@@ -16,6 +22,7 @@ const getNextLine = (line) =>
   vscode.window.activeTextEditor.document.lineAt(
     (typeof line !== "number" ? line.lineNumber : line) + 1
   )
+
 /**
  * @param {vscode.TextEditor} editor - context
  * @returns {boolean} is any text selected
@@ -23,15 +30,25 @@ const getNextLine = (line) =>
 const hasSelection = (editor) =>
   !editor.selection.active.isEqual(editor.selection.anchor)
 
-const JSDOC_START_TAG = /\/\*\*\s?/
-const JSDOC_END_TAG = /\s?\*\//
+/**
+ * @param {vscode.TextLine} line - input
+ * @returns {vscode.Position} first non whitespace position
+ */
+const getContentStartPos = (line) =>
+  new vscode.Position(line.lineNumber, line.firstNonWhitespaceCharacterIndex)
+
+/**
+ * @param {vscode.TextLine} line - input
+ * @returns {vscode.Position} first non whitespace position
+ */
+const getContentEndPos = (line) => line.range.end
 
 /**
  * @param {vscode.ExtensionContext} context - default param
  */
 function activate(context) {
   const disposable = vscode.commands.registerCommand(
-    "jsdoc-commenter.toggle",
+    "jsdoc-comment-toggler.toggle",
     async () => {
       const editor = vscode.window.activeTextEditor
       if (!editor) return
@@ -62,7 +79,7 @@ function activate(context) {
         }
       }
 
-      // remove jsdoc tags (preserves comment body)
+      // #region - rm jsdoc tags (preserves comment body)
       if (jsdocStart && jsdocEnd) {
         editor.edit((editBuilder) => {
           // single line comment, no selection & selection
@@ -92,14 +109,14 @@ function activate(context) {
           editBuilder.delete(lineFirst.rangeIncludingLineBreak)
           editBuilder.delete(lineLast.rangeIncludingLineBreak)
 
-          // continuation comment line *'s
+          // continuation comment line's *s
           for (
             let i = lineFirst.lineNumber + 1;
             i <= lineLast.lineNumber - 1;
             i += 1
           ) {
             const line = editor.document.lineAt(i)
-            const jsdocComment = line.text.match(/\s\*\s/)
+            const jsdocComment = line.text.match(JSDOC_LINE_CHAR)
             editBuilder.delete(
               new vscode.Range(
                 line.lineNumber,
@@ -114,9 +131,7 @@ function activate(context) {
         return
       }
 
-      // TODO: wrap exact selection when it's only 1 line
-
-      // add comment tags
+      // #region - add jsdoc tags
       const wasInsertSuccessful = await editor.edit((editBuilder) => {
         // single line comment
         if (lineFirst.lineNumber === lineLast.lineNumber) {
@@ -125,14 +140,8 @@ function activate(context) {
             editBuilder.insert(editor.selection.end, " */")
           } else {
             // no selection
-            const contentStart = new vscode.Position(
-              lineFirst.lineNumber,
-              lineFirst.firstNonWhitespaceCharacterIndex
-            )
-            const contentEnd = lineLast.range.end
-
-            editBuilder.insert(contentStart, "/** ")
-            editBuilder.insert(contentEnd, " */")
+            editBuilder.insert(getContentStartPos(lineFirst), "/** ")
+            editBuilder.insert(getContentEndPos(lineLast), " */")
           }
 
           return
@@ -146,36 +155,27 @@ function activate(context) {
           i += 1
         ) {
           const line = editor.document.lineAt(i)
-          // const jsdocComment = line.text.match(/\\s\\+s/)
-          const contentStart = new vscode.Position(
-            line.lineNumber,
-            line.firstNonWhitespaceCharacterIndex
-          )
-
-          editBuilder.insert(contentStart, " * ")
+          editBuilder.insert(getContentStartPos(line), " * ")
         }
 
-        const contentStart = new vscode.Position(
-          lineFirst.lineNumber,
-          lineFirst.firstNonWhitespaceCharacterIndex
-        )
-        const contentEnd = lineLast.range.end
         const indentation = " ".repeat(
           lineFirst.firstNonWhitespaceCharacterIndex
         )
 
-        editBuilder.insert(contentStart, `/**\n${indentation} * `)
-        editBuilder.insert(contentEnd, `\n${indentation} */`)
+        editBuilder.insert(
+          getContentStartPos(lineFirst),
+          `/**\n${indentation} * `
+        )
+        editBuilder.insert(getContentEndPos(lineLast), `\n${indentation} */`)
       })
 
       // if cursor was at end of last line, the comment tag is errantly placed
       // before the cursor. this moves the comment tag after the cursor, keeping the
       // cursor in the original position before the JSDoc was added
       // vscode doesn't have the ability to add to line index greater than max
-      const cursorPos = editor.selection.active
-      const anchorPos = editor.selection.anchor
-
+      // #region - fix cursor / selection pos
       if (!wasInsertSuccessful) return
+      const cursorPos = editor.selection.active
 
       // single line comment
       if (lineFirst.lineNumber === lineLast.lineNumber) {
@@ -189,7 +189,7 @@ function activate(context) {
             isCursorAtEnd ? adjustedSelectionEndPos : editor.selection.start
           )
         } else if (
-          cursorPos.isEqual(editor.document.lineAt(cursorPos).range.end)
+          cursorPos.isEqual(getContentEndPos(editor.document.lineAt(cursorPos)))
         ) {
           // no selection but cursor at end of line
           editor.selection = new vscode.Selection(
@@ -202,13 +202,14 @@ function activate(context) {
         // selection ends at end of line
         if (
           editor.selection.end.isAfterOrEqual(
-            editor.document.lineAt(editor.selection.end).range.end
+            getContentEndPos(editor.document.lineAt(editor.selection.end))
           )
         ) {
-          const adjustedSelectionEndPos = getPrevLine(editor.selection.end.line)
-            .range.end
+          const adjustedSelectionEndPos = getContentEndPos(
+            getPrevLine(editor.selection.end.line)
+          )
 
-          // ensure cursor is at same side of selection
+          // ensure cursor is at original side of the selection
           const isCursorAtEnd = cursorPos.isEqual(editor.selection.end)
           editor.selection = new vscode.Selection(
             isCursorAtEnd ? editor.selection.start : adjustedSelectionEndPos,
@@ -219,21 +220,15 @@ function activate(context) {
         // selection starts before first non-whitespace char of line
         if (
           editor.selection.start.isBefore(
-            new vscode.Position(
-              editor.selection.start.line,
-              editor.document.lineAt(
-                editor.selection.start
-              ).firstNonWhitespaceCharacterIndex
-            )
+            getContentStartPos(editor.document.lineAt(editor.selection.start))
           )
         ) {
           const adjustedSelectionStartLine = getNextLine(
             editor.selection.start.line
           )
-          const adjustedSelectionStartPos = new vscode.Position(
-            adjustedSelectionStartLine.lineNumber,
-            adjustedSelectionStartLine.firstNonWhitespaceCharacterIndex + 2
-          )
+          const adjustedSelectionStartPos = getContentStartPos(
+            adjustedSelectionStartLine
+          ).translate(0, 2)
 
           // ensure cursor is at same side of selection
           const isCursorAtStart = cursorPos.isEqual(editor.selection.start)

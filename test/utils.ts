1378b814350802910bbd86145cc9f4722302b540
Zach Hardesty <hello@zachhardesty.com>
2020-10-13 17:39:37 -0500

    docs: rm unnecessary jsdoc in ts file

--- test/utils.ts
+++ test/utils.ts
@@ -5,7 +5,7 @@ export const VERBOSE = true
 /**
  * simple wrapper around `console.log` that only logs when `VERBOSE` is true
  *
- * @param {...any} messages - items to print to console
+ * @param messages - items to print to console
  */
 export const log = (...messages: unknown[]): void => {
   if (VERBOSE) console.log(...messages)
@@ -15,7 +15,7 @@ export const log = (...messages: unknown[]): void => {
  * simple wrapper around `console.info` that only logs when `VERBOSE` is true and
  * prefixes logged items with cyan colored string `"info"`
  *
- * @param {...any} messages - items to print to console
+ * @param messages - items to print to console
  */
 log.info = (...messages: unknown[]): void => {
   if (VERBOSE) console.info(cyan("info"), ...messages)
@@ -25,7 +25,7 @@ log.info = (...messages: unknown[]): void => {
  * simple wrapper around `console.warn` that only logs when `VERBOSE` is true and
  * prefixes logged items with bright yellow colored string `"warn"`
  *
- * @param {...any} messages - items to print to console
+ * @param messages - items to print to console
  */
 log.warn = (...messages: unknown[]): void => {
   if (VERBOSE) console.warn(yellowBright("warn"), ...messages)
@@ -35,7 +35,7 @@ log.warn = (...messages: unknown[]): void => {
  * simple wrapper around `console.error` that only logs when `VERBOSE` is true and
  * prefixes logged items with red colored string `"error"`
  *
- * @param {...any} messages - items to print to console
+ * @param messages - items to print to console
  */
 log.error = (...messages: unknown[]): void => {
   if (VERBOSE) console.error(red("error"), ...messages)

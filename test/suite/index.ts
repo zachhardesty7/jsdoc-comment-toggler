6a733b5bfb74d3395a80c0da6d0d38bec29b107f
Zach Hardesty <hello@zachhardesty.com>
2022-02-27 00:20:55 -0600

    chore: fix errant test import style change

--- test/suite/index.ts
+++ test/suite/index.ts
@@ -1,9 +1,9 @@
 /* eslint-disable import/no-unresolved, import/extensions */
 
-import * as path from "path"
-import * as Mocha from "mocha"
-import * as glob from "glob"
-import * as vscode from "vscode"
+import path from "path"
+import Mocha from "mocha"
+import glob from "glob"
+import vscode from "vscode"
 import { cyan } from "ansi-colors"
 import { log } from "../utils"
 
